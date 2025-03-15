aedffbc560d672d133d6ebbdc65a6d06e2fa2bb7
Paloma <149347087+PalomaProject@users.noreply.github.com>
2025-03-15 12:44:36 -0300

    Desafio 2
    Desafio 2: faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
    na tela

---
+++ desafio 2.py
@@ -0,0 +1,5 @@
+"""Desafio 2: faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
+na tela"""
+
+nome = input('digite seu nome'))
+print('é um prazer te conhecer',nome)
