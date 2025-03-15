6ccab467a3098e300275bb6340fe959d12a91701
Paloma <149347087+PalomaProject@users.noreply.github.com>
2025-03-15 12:47:22 -0300

    Desafio 3: leia 2 numeros e mostre as somas
    Desafio 3: crie um programa que leia dois numeros e mostre as somas entre eles

---
+++ desafio 3.py
@@ -0,0 +1,9 @@
+"""Desafio 3: crie um programa que leia dois numeros e mostre as somas entre eles """
+
+
+numero1 = int(input('digite um numero'))
+numero2 = int(input('digite mais um numero'))
+
+soma = numero1 + numero2
+
+print(' a soma entre {} e {} é igual a {}' .format(numero1,numero2,soma))
