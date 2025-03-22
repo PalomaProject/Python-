2df1bb1ba6073288b4e75dda74b171d656060551
Paloma <149347087+PalomaProject@users.noreply.github.com>
2025-03-21 23:43:49 -0300

    desafio 5: leia um numero inteiro e mostre na tela o seu antecessor e o seu sucessor
    desafio 5: faça um programa que leia um numero inteiro e mostre na tela o seu antecessor e o seu
    sucessor

---
+++ desafio 5 antecessor e sucessor.py
@@ -0,0 +1,7 @@
+""" desafio 5: faça um programa que leia um numero inteiro e mostre na tela o seu antecessor e o seu
+sucessor"""
+
+numero = (int(input('digite um numero')))
+somar = numero + 1
+diminuir = numero - 1
+print('analisando o valor {}, seu antecessor é {} o seu sucessor é {}' .format(numero,diminuir,somar))
