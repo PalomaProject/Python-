363e90da556b3a3efa0272211391fec287567bad
Paloma <149347087+PalomaProject@users.noreply.github.com>
2025-03-22 22:46:00 -0300

    Desafio 6:
    Desafio 6: crie um programa que leia um numero e mostre seu dobro, triplo e a raiz quadrada

---
+++ desafio 6 - treinar raiz quadrada.py
@@ -0,0 +1,13 @@
+"""Desafio 6: crie um programa que leia um numero e mostre seu dobro, triplo e a raiz quadrada"""
+
+numero = int(input('digite um numero'))
+
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** (1/2)
+
+print('o dobro do numero digitado é {}, o triplo é {} e a raiz quadrada é {:.3f}' .format(dobro,triplo,raiz))
+
+
+
+#mais um que não conseguir fazer só, PRECISO PRATICAR O CALCULO DE RAIZ QUADRADA EM PYTHON!
