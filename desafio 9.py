d9a0390c31a1127b5483dfb408e5e265ce230a86
Paloma <149347087+PalomaProject@users.noreply.github.com>
2025-03-26 12:40:48 -0300

    Desafio 9: mostre na tela sua tabuada
    Desafio 9: crie um programa que leia um numero inteiro e mostre na tela sua tabuada

---
+++ desafio 9.py
@@ -0,0 +1,26 @@
+"""Desafio 9: crie um programa que leia um numero inteiro e mostre na tela sua tabuada"""
+
+numero = int(input('digite um numero de 1 a 10'))
+
+print ('a tabuada desse numero é:')
+
+n1 = numero*1
+print ('{} x 1 = {}' .format(numero,n1))
+n2 = numero*2
+print ('{} x 2 = {}' .format(numero,n2))
+n3 = numero*3
+print ('{} x 3 = {}' .format(numero,n3))
+n4 = numero*4
+print ('{} x 4 = {}' .format(numero,n4))
+n5 = numero*5
+print ('{} x 5 = {}' .format(numero,n5))
+n6 = numero*6
+print ('{} x 6 = {}' .format(numero,n6))
+n7 = numero*7
+print ('{} x 7 = {}' .format(numero,n7))
+n8 = numero*8
+print ('{} x 8 = {}' .format(numero,n8))
+n9 = numero*9
+print ('{} x 9 = {}' .format(numero,n9))
+n10 = numero*10
+print ('{} x 10 = {}' .format(numero,n10))
