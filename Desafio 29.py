14b967fe67b86cb919c3b7213f46858b73970d7c
Paloma <149347087+PalomaProject@users.noreply.github.com>
2025-04-11 13:04:51 -0300

    Desafio 29: programa que leia a velocidade
    Desafio 29: escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele
    foi multado. A multa vai custar R$ 7,00 por cada km acima do limite.

---
+++ Desafio 29.py
@@ -0,0 +1,14 @@
+'''Desafio 29: escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele
+foi multado. A multa vai custar R$ 7,00 por cada km acima do limite.'''
+
+
+velocidade = float(input('qual era a velocidade do seu carro?'))
+
+multa = (velocidade - 80) * 7
+
+if velocidade <= 80:
+    print('pode seguir')
+else:
+    print('multado, valor a pagar R${}' .format(multa))
+
+    print('FIM')
