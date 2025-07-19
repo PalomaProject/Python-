0b13f01b9bd307c86f3dbfca9b59e73e6eacf518
Paloma <149347087+PalomaProject@users.noreply.github.com>
2025-07-19 14:16:33 -0300

    desafio 47: Contagem de pares
    Desafio 47: crie um programa que mostre na tela todos os numeros pares que estão no intervalo
    entre 1 e 50

---
+++ desafio 47 Contagem de pares.py
@@ -0,0 +1,7 @@
+"""Desafio 47: crie um programa que mostre na tela todos os numeros pares que estão no intervalo
+entre 1 e 50"""
+
+
+for intervalo in range(0,51,2):
+    print (intervalo, end=' ')
+print('FIM')
